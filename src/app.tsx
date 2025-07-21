7926779fdb5c9911f67cf2def783da81d73defa0
Daniel Ström <D@nielstrom.se>
2025-07-21 22:41:51 +0200

    Set up boilerplate

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import PWABadge from "./PWABadge.tsx"
+import "./app.css"
+
+export const App = () => (
+  <>
+    <h1 className="text-2xl">StatStash</h1>
+
+    <PWABadge />
+  </>
+)
---
+++ vite.config.ts
@@ -0,0 +1,41 @@
+import { VitePWA } from "vite-plugin-pwa"
+import { defineConfig } from "vite"
+import preact from "@preact/preset-vite"
+import tailwindcss from "@tailwindcss/vite"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    preact(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "prompt",
+      injectRegister: false,
+
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+
+      manifest: {
+        name: "StatStash",
+        short_name: "StatStash",
+        description: "Stores your character sheets",
+        theme_color: "#000000",
+      },
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+
+      devOptions: {
+        enabled: false,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
+})
