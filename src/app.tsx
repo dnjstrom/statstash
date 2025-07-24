585e7591173a34475f2362728ec89a7fea717352
Daniel Ström <D@nielstrom.se>
2025-07-24 15:18:12 +0200

    Handle base path in dev and prod differently

--- src/app.tsx
+++ src/app.tsx
@@ -13,14 +13,22 @@ import { StatsProvider } from "./useStats.tsx"
 
 const NotFound = lazy(() => import("./NotFound.tsx").then((m) => m.NotFound))
 
+const pathWithBase = (path: string): string => {
+  if (import.meta.env.PROD) {
+    return `/statstash/${path}`
+  }
+
+  return path
+}
+
 export const App = () => {
   return (
     <LocationProvider>
       <ErrorBoundary>
         <StatsProvider>
           <Router>
-            <Route path="/statstash" component={Home} />
-            <Route path="/statstash/dnd" component={DNDLayout} />
+            <Route path={pathWithBase("/")} component={DNDLayout} />
+            <Route path={pathWithBase("/stats")} component={Home} />
             <Route default component={NotFound} />
             <PWABadge />
           </Router>
