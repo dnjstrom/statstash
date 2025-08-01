96dc9bf80311914975cbd9b4d0965b4970e958d7
Daniel Ström <D@nielstrom.se>
2025-08-01 15:09:05 +0200

    Make proficiencies more highlighted

--- src/dnd.tsx
+++ src/dnd.tsx
@@ -389,7 +389,9 @@ const SkillBox = ({
       className="flex gap-2 items-baseline px-4 py-2 bg-[oklch(0.2507_0.0321_232.15)] cursor-pointer hover:bg-[oklch(0.2707_0.0321_232.15)] active:bg-[oklch(0.2307_0.0321_232.15)]"
       onClick={onClick}
     >
-      <div>{proficient ? "●" : "○"}</div>
+      <div className="flex justify-center items-center text-xl w-2 h-2">
+        {proficient ? <span className="text-cyan-600">●</span> : "○"}
+      </div>
       {name}
       <div className="ml-auto">
         <Stat value={modifier} />
